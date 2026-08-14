b83dd00406ca096d798f9b2eafc02abd2fde99e5
Ömer TEKTAŞ <bybrawe@outlook.com>
2026-08-14 03:38:12 +0300

    feat: bridge stable Loop into current V2 server plugin host

--- src/source/opencode2/experimental.js
+++ src/source/opencode2/experimental.js
@@ -1,8 +1,8 @@
-import { define } from "@opencode-ai/plugin/v2/promise"
+import OpenCodeLoopPlugin from "../../index.js"
 
 export const OPENCODE2_LOOP_PLUGIN_ID = "bybrawe.opencode-loop.v2"
 
-export default define({
+export default {
   id: OPENCODE2_LOOP_PLUGIN_ID,
-  setup: async () => {},
-})
+  server: OpenCodeLoopPlugin,
+}
